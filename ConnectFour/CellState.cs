88a88d55c220a03de9d075ad1e9b632f578946d6
chanh <chanheng.chh@gmail.com>
2026-04-19 13:32:09 -0400

    clean update

--- ConnectFour/CellState.cs
+++ ConnectFour/CellState.cs
@@ -1,15 +1,10 @@
 namespace ConnectFour;
 
-/// <summary>
 /// Represents the state of a single cell on the Connect Four board.
-/// </summary>
 public enum CellState
 {
-    /// <summary>No piece in this cell.</summary>
     Empty,
-    /// <summary>Player 1's piece (displayed as 'X').</summary>
     Player1,
-    /// <summary>Player 2's piece (displayed as 'O').</summary>
     Player2
 }
 
